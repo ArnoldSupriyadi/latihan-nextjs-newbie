eca0a16eadfe30d70dbe40055b7a3439a2275748
ArnoldSupriyadi <arnoldsupriyadi@gmail.com>
2026-05-07 06:43:53 +0700

    baru sampai belajar route, layouting

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "About Page",
+  description: "This is the about page of the Next.js application.",
+};
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is the about page of our Next.js application.</p>
+        </div>
+    )
+}
